a6be7ac242a144a6c13519d717b7694e1d5ffc8a
78Istvan <istvan78juhasz@gmail.com>
2021-06-14 22:59:04 +0000

    add a function match to check are they the same or nut, and the timeout to turn back if they are not match

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,44 @@
 //flip the cards//
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCrad() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return
 }
+
+secondCard = this;
+hasFlippedCard = false;
+
+checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCrad);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
 cards.forEach(card => card.addEventListener('click', flipCrad))
-""
+
 
