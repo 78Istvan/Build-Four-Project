b907ef10e5ccacce963e6a592afa732e82529983
78Istvan <istvan78juhasz@gmail.com>
2021-06-15 23:34:09 +0000

    cards get shuffle function and reset and button and count moves option

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,11 @@
 const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCrad() {
+    if (lockBoard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -18,26 +20,36 @@ hasFlippedCard = false;
 
 checkForMatch();
 }
-
+//checks cards are matching//
 function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    if (firstCard.dataset.name === secondCard.dataset.name) {
         disableCards();
         return
     }
     unflipCards();
 }
-
+//match cards don`t flip back if clicked//
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCrad);
 }
 
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard = false;
     }, 1500);
 }
+
+(function shuffle() {
+    cards.forEach(card => {
+        let shuffPosi = Math.floor(Math.random() * 16);
+        card.style.order = shuffPosi;
+        console.log(shuffle)
+    })
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCrad))
 
